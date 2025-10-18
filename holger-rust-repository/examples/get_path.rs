69ab044e255a1e0a12468c02354267b133dda2f1
Hatchepsut <peter@hatchepsut.se>
2025-10-18 11:24:43 +0200

    Added example demonstrating sparse_path to tuple conversion in RustRepo

--- holger-rust-repository/examples/get_path.rs
+++ holger-rust-repository/examples/get_path.rs
@@ -1,4 +1,7 @@
 use holger_rust_repository::RustRepo;
+
+type RPath<'a> = (&'a str, &'a str, &'a str);
+
 fn main() {
     //let repo = RustRepo::new("kalle".to_string());
     println!("path: {:?}", RustRepo::sparse_path("a"));
@@ -6,7 +9,7 @@ fn main() {
     println!("path: {:?}", RustRepo::sparse_path("abc"));
     println!("path: {:?}", RustRepo::sparse_path("tokio"));
 
-    let tuple_path: (&str, &str, &str) = RustRepo::sparse_path("tokio").into();
+    let tuple_path: RPath = RustRepo::sparse_path("tokio").into();
 
     println!("Tuple: {:?}", tuple_path);
 }
