ee5c6ff58530103af167471128949bc2abae265e
Rickard Lundin <rickard@ignalina.dk>
2026-05-19 20:30:14 +0200

    moving

--- holger-ron/src/repository/types.rs
+++
@@ -1,8 +0,0 @@
-use std::any::Any;
-use std::collections::HashMap;
-use std::sync::Arc;
-use serde::{Deserialize, Serialize};
-
-use async_trait::async_trait;
-
-
--- holger/src/main.rs
+++
@@ -1,6 +0,0 @@
-use tokio::main;
-
-#[tokio::main]
-async fn main() -> anyhow::Result<()> {
-    holger_cli::run().await
-}
